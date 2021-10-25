c89ab4a3e0684e447afce743ba2c642a6003225f
Geov94 <agsalamanca@misena.edu.co>
2021-10-25 12:50:12 -0500

    finalizado el modulo de arrays

--- Arrays/fullClasesArrays.js
+++ Arrays/fullClasesArrays.js
@@ -3,25 +3,29 @@
 var geovani = { 
     nombre:'Andres',
     apellido:'Salamanca',
-    altura: 1.74
+    altura: 1.74,
+    cantidadDeLibros: 30
  }
 
  var julian = {
      nombre: 'Julian',
      apellido:'Salamanca',
-     altura: 1.20
+     altura: 1.20,
+     cantidadDeLibros: 5
  }
  
 var gabriel = { 
     nombre:'Gabriel',
     apellido:'Batistuta',
-    altura: 1.85
+    altura: 1.85,
+    cantidadDeLibros: 11
  }
 
  var javier = {
      nombre: 'Javier',
      apellido:'Zanetti',
-     altura: 1.78
+     altura: 1.78,
+     cantidadDeLibros: 18
  }
 
 
@@ -46,10 +50,33 @@ const esAlta = ({altura}) => altura >1.8;
 // var personasAltas = personas.filter(function(persona){
 
 //     return persona.altura > 1.8;
+
 // })
 
- console.log (personasAltas);
+//map
+const pasarAlturaAcms = persona =>({
+   
+        ...persona,
+        altura:persona.altura*100
+  
+})
 
 
+
+
+
+var personasCms = personas.map(pasarAlturaAcms)
+
+
+ console.log (personasCms);
+
+
+//reduce
+
+const reducer = (acum,{cantidadDeLibros})=>  acum+cantidadDeLibros;
+
+var totalDeLibros = personas.reduce(reducer,0)
+
+console.log(`en total tienen ${totalDeLibros} libros`)
 
 
